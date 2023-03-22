81c6b50040b93bf179fbb5726eea6fc97e236802
Munna Kumar Barnwal <barnwal.munna@tftus.com>
2023-03-22 11:34:30 +0530

    added style to login and register page

---
+++ assets/register.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("create_customer");
+console.log("register page")
+// form.addEventListener("submit",(e) => {
+//     e.preventDefault()
+//     console.log("form submit event happened")
+// })
