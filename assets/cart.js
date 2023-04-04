afc295bb313fadc3449cc69e37e697b109dce7ca
Munna Kumar Barnwal <barnwal.munna@tftus.com>
2023-04-04 23:26:52 +0530

    update cart quatity and delete cart feature customized

--- assets/cart.js
+++ assets/cart.js
@@ -1,212 +1,134 @@
-class CartRemoveButton extends HTMLElement {
-  constructor() {
-    super();
+const cartCountElement = document.querySelector('.cart-count-bubble')
+const cartItems = document.querySelectorAll('.cart-item');
+const format = document.querySelector('[data-money-format]').getAttribute('data-money-format')
 
-    this.addEventListener('click', (event) => {
-      event.preventDefault();
-      const cartItems = this.closest('cart-items') || this.closest('cart-drawer-items');
-      cartItems.updateQuantity(this.dataset.index, 0);
-    });
+cartItems.forEach((cartItem) => {
+  const minusBtn = cartItem.querySelector('[name="minus"]')
+  const plusBtn = cartItem.querySelector('[name="plus"]')
+  const quantityInput = cartItem.querySelector('.quantity__input')
+  const lineItemKey = cartItem.getAttribute('data-line-item-key')
+  const loaderElement = cartItem.querySelector('.loading-spinner')
+  const removeElement = cartItem.querySelector('.cart__remove__icon')
+
+  plusBtn.addEventListener('click',() => {
+    quantityInput.value = quantityInput.valueAsNumber+1
+    changeItemQuantity(lineItemKey,quantityInput.valueAsNumber,loaderElement)
+  })
+
+  minusBtn.addEventListener('click',() => {
+    quantityInput.value = quantityInput.valueAsNumber === 1 ? 1 : quantityInput.valueAsNumber-1
+    changeItemQuantity(lineItemKey,quantityInput.valueAsNumber,loaderElement)
+  })
+
+  removeElement.addEventListener('click',(e) => {
+    e.preventDefault()
+    // remove ajax api functionality
+    removeItem(lineItemKey,cartItem,loaderElement)
+  })
+
+})
+
+const changeItemQuantity = async(key, quantity, loaderElement) => {
+  const qtyData = {id:key,quantity}
+
+  const config = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(qtyData)
+  }
+
+  try {
+    loaderElement.classList.add('show-loading-spinner')
+    const res = await fetch("/cart/change.js",config)
+    const cart = await res.json()
+    
+    const item = cart.items.find(item => item.key === key)
+
+    cartCountElement.textContent = cart.item_count
+    document.querySelector('.totals__subtotal-value').textContent = moneyFormatter(cart.total_price,format)
+    document.querySelector(`[data-line-item-key="${key}"] .line-item-price`).textContent = moneyFormatter(item.final_line_price,format)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loaderElement.classList.remove('show-loading-spinner')
   }
 }
 
-customElements.define('cart-remove-button', CartRemoveButton);
+const removeItem = async(key,cartItem,loaderElement) => {
+  const qtyData = {id:key,quantity:0}
 
-class CartItems extends HTMLElement {
-  constructor() {
-    super();
-    this.lineItemStatusElement = document.getElementById('shopping-cart-line-item-status') || document.getElementById('CartDrawer-LineItemStatus');
-
-    const debouncedOnChange = debounce((event) => {
-      this.onChange(event);
-    }, ON_CHANGE_DEBOUNCE_TIMER);
-
-    this.addEventListener('change', debouncedOnChange.bind(this));
+  const config = {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(qtyData)
   }
 
-  cartUpdateUnsubscriber = undefined;
+  try {
+    loaderElement.classList.add('show-loading-spinner')
+    const res = await fetch("/cart/change.js",config)
+    const cart = await res.json()
 
-  connectedCallback() {
-    this.cartUpdateUnsubscriber = subscribe(PUB_SUB_EVENTS.cartUpdate, (event) => {
-      if (event.source === 'cart-items') {
-        return;
-      }
-      this.onCartUpdate();
-    });
-  }
+    cartItem.remove()
 
-  disconnectedCallback() {
-    if (this.cartUpdateUnsubscriber) {
-      this.cartUpdateUnsubscriber();
+    if(cart.items.length === 0) {
+      // show empty cart text and empty cart count inthe header section
+      cartCountElement.textContent = ''
+      document.querySelector('#main-cart-footer').classList.add("is-empty")
+      document.querySelector('#main-cart-items').classList.add("is-empty")
     }
-  }
 
-  onChange(event) {
-    this.updateQuantity(event.target.dataset.index, event.target.value, document.activeElement.getAttribute('name'));
-  }
-
-  onCartUpdate() {
-    fetch(`${routes.cart_url}?section_id=main-cart-items`)
-      .then((response) => response.text())
-      .then((responseText) => {
-        const html = new DOMParser().parseFromString(responseText, 'text/html');
-        const sourceQty = html.querySelector('cart-items');
-        this.innerHTML = sourceQty.innerHTML;
-      })
-      .catch(e => {
-        console.error(e);
-      });
-  }
-
-  getSectionsToRender() {
-    return [
-      {
-        id: 'main-cart-items',
-        section: document.getElementById('main-cart-items').dataset.id,
-        selector: '.js-contents'
-      },
-      {
-        id: 'cart-icon-bubble',
-        section: 'cart-icon-bubble',
-        selector: '.shopify-section'
-      },
-      {
-        id: 'cart-live-region-text',
-        section: 'cart-live-region-text',
-        selector: '.shopify-section'
-      },
-      {
-        id: 'main-cart-footer',
-        section: document.getElementById('main-cart-footer').dataset.id,
-        selector: '.js-contents'
-      }
-    ];
-  }
-
-  updateQuantity(line, quantity, name) {
-    this.enableLoading(line);
-
-    const body = JSON.stringify({
-      line,
-      quantity,
-      sections: this.getSectionsToRender().map((section) => section.section),
-      sections_url: window.location.pathname
-    });
-
-    fetch(`${routes.cart_change_url}`, { ...fetchConfig(), ...{ body } })
-      .then((response) => {
-        return response.text();
-      })
-      .then((state) => {
-        const parsedState = JSON.parse(state);
-        const quantityElement = document.getElementById(`Quantity-${line}`) || document.getElementById(`Drawer-quantity-${line}`);
-        const items = document.querySelectorAll('.cart-item');
-
-        if (parsedState.errors) {
-          quantityElement.value = quantityElement.getAttribute('value');
-          this.updateLiveRegions(line, parsedState.errors);
-          return;
-        }
-
-        this.classList.toggle('is-empty', parsedState.item_count === 0);
-        const cartDrawerWrapper = document.querySelector('cart-drawer');
-        const cartFooter = document.getElementById('main-cart-footer');
-
-        if (cartFooter) cartFooter.classList.toggle('is-empty', parsedState.item_count === 0);
-        if (cartDrawerWrapper) cartDrawerWrapper.classList.toggle('is-empty', parsedState.item_count === 0);
-
-        this.getSectionsToRender().forEach((section => {
-          const elementToReplace =
-            document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
-          elementToReplace.innerHTML =
-            this.getSectionInnerHTML(parsedState.sections[section.section], section.selector);
-        }));
-        const updatedValue = parsedState.items[line - 1] ? parsedState.items[line - 1].quantity : undefined;
-        let message = '';
-        if (items.length === parsedState.items.length && updatedValue !== parseInt(quantityElement.value)) {
-          if (typeof updatedValue === 'undefined') {
-            message = window.cartStrings.error;
-          } else {
-            message = window.cartStrings.quantityError.replace('[quantity]', updatedValue);
-          }
-        }
-        this.updateLiveRegions(line, message);
-
-        const lineItem = document.getElementById(`CartItem-${line}`) || document.getElementById(`CartDrawer-Item-${line}`);
-        if (lineItem && lineItem.querySelector(`[name="${name}"]`)) {
-          cartDrawerWrapper ? trapFocus(cartDrawerWrapper, lineItem.querySelector(`[name="${name}"]`)) : lineItem.querySelector(`[name="${name}"]`).focus();
-        } else if (parsedState.item_count === 0 && cartDrawerWrapper) {
-          trapFocus(cartDrawerWrapper.querySelector('.drawer__inner-empty'), cartDrawerWrapper.querySelector('a'))
-        } else if (document.querySelector('.cart-item') && cartDrawerWrapper) {
-          trapFocus(cartDrawerWrapper, document.querySelector('.cart-item__name'))
-        }
-        publish(PUB_SUB_EVENTS.cartUpdate, {source: 'cart-items'});
-      }).catch(() => {
-        this.querySelectorAll('.loading-overlay').forEach((overlay) => overlay.classList.add('hidden'));
-        const errors = document.getElementById('cart-errors') || document.getElementById('CartDrawer-CartErrors');
-        errors.textContent = window.cartStrings.error;
-      })
-      .finally(() => {
-        this.disableLoading(line);
-      });
-  }
-
-  updateLiveRegions(line, message) {
-    const lineItemError = document.getElementById(`Line-item-error-${line}`) || document.getElementById(`CartDrawer-LineItemError-${line}`);
-    if (lineItemError) lineItemError.querySelector('.cart-item__error-text').innerHTML = message;
-
-    this.lineItemStatusElement.setAttribute('aria-hidden', true);
-
-    const cartStatus = document.getElementById('cart-live-region-text') || document.getElementById('CartDrawer-LiveRegionText');
-    cartStatus.setAttribute('aria-hidden', false);
-
-    setTimeout(() => {
-      cartStatus.setAttribute('aria-hidden', true);
-    }, 1000);
-  }
-
-  getSectionInnerHTML(html, selector) {
-    return new DOMParser()
-      .parseFromString(html, 'text/html')
-      .querySelector(selector).innerHTML;
-  }
-
-  enableLoading(line) {
-    const mainCartItems = document.getElementById('main-cart-items') || document.getElementById('CartDrawer-CartItems');
-    mainCartItems.classList.add('cart__items--disabled');
-
-    const cartItemElements = this.querySelectorAll(`#CartItem-${line} .loading-overlay`);
-    const cartDrawerItemElements = this.querySelectorAll(`#CartDrawer-Item-${line} .loading-overlay`);
-
-    [...cartItemElements, ...cartDrawerItemElements].forEach((overlay) => overlay.classList.remove('hidden'));
-
-    document.activeElement.blur();
-    this.lineItemStatusElement.setAttribute('aria-hidden', false);
-  }
-
-  disableLoading(line) {
-    const mainCartItems = document.getElementById('main-cart-items') || document.getElementById('CartDrawer-CartItems');
-    mainCartItems.classList.remove('cart__items--disabled');
-
-    const cartItemElements = this.querySelectorAll(`#CartItem-${line} .loading-overlay`);
-    const cartDrawerItemElements = this.querySelectorAll(`#CartDrawer-Item-${line} .loading-overlay`);
-
-    cartItemElements.forEach((overlay) => overlay.classList.add('hidden'));
-    cartDrawerItemElements.forEach((overlay) => overlay.classList.add('hidden'));
+    document.querySelector('.totals__subtotal-value').textContent = moneyFormatter(cart.total_price,format)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loaderElement.classList.remove('show-loading-spinner')
   }
 }
 
-customElements.define('cart-items', CartItems);
+function moneyFormatter(cents, format) {
+  if (typeof cents == 'string') { cents = cents.replace('.',''); }
+  var value = '';
+  var placeholderRegex = /\{\{\s*(\w+)\s*\}\}/;
+  var formatString = (format || this.money_format);
 
-if (!customElements.get('cart-note')) {
-  customElements.define('cart-note', class CartNote extends HTMLElement {
-      constructor() {
-        super();
+  function defaultOption(opt, def) {
+     return (typeof opt == 'undefined' ? def : opt);
+  }
 
-      this.addEventListener('change', debounce((event) => {
-            const body = JSON.stringify({ note: event.target.value });
-            fetch(`${routes.cart_update_url}`, { ...fetchConfig(), ...{ body } });
-      }, ON_CHANGE_DEBOUNCE_TIMER))
-      }
-  });
+  function formatWithDelimiters(number, precision, thousands, decimal) {
+    precision = defaultOption(precision, 2);
+    thousands = defaultOption(thousands, ',');
+    decimal   = defaultOption(decimal, '.');
+
+    if (isNaN(number) || number == null) { return 0; }
+
+    number = (number/100.0).toFixed(precision);
+
+    var parts   = number.split('.'),
+        dollars = parts[0].replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1' + thousands),
+        cents   = parts[1] ? (decimal + parts[1]) : '';
+
+    return dollars + cents;
+  }
+
+  switch(formatString.match(placeholderRegex)[1]) {
+    case 'amount':
+      value = formatWithDelimiters(cents, 2);
+      break;
+    case 'amount_no_decimals':
+      value = formatWithDelimiters(cents, 0);
+      break;
+    case 'amount_with_comma_separator':
+      value = formatWithDelimiters(cents, 2, '.', ',');
+      break;
+    case 'amount_no_decimals_with_comma_separator':
+      value = formatWithDelimiters(cents, 0, '.', ',');
+      break;
+  }
+
+  return formatString.replace(placeholderRegex, value);
 };
